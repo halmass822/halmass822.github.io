6f0b346b5397d80de9ae70486754821e42c2e2d3
Hazem Al Massri <halmassri822@gmail.com>
2022-04-23 17:43:38 -0400

    completed main logic, added images

--- minesweeper.js
+++ minesweeper.js
@@ -20,9 +20,11 @@
             let YCoordinate = coordinate.slice(2,4);
             return!(XCoordinate < 1 || XCoordinate > gridWidth || YCoordinate < 1 || YCoordinate > gridHeight)
         }
-
-        function getSurroundingTiles(){
+        //returns an array of all the coordinates of the surrounding tiles, automatically omitting invalid coordinates (like at corners)
+        function getSurroundingTiles(centerCoord){
             //array of the relative positions of the surrounding tiles
+            const XCoord = Number(centerCoord.slice(0,2));
+            const YCoord = Number(centerCoord.slice(2,4));
             const surroundingTilesArray = [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]];
             let outputArray = [];
             surroundingTilesArray.forEach((x) => {
@@ -37,6 +39,64 @@
             return outputArray;
         }
 
+        //changes the related tile element to show the tile contents at the appropriate color
+        function revealTile(coordinate, content) {
+            let outputColor;
+            switch(content) {
+                case "1":
+                    outputColor = "blue";
+                    document.getElementById(coordinate).innerText = content; 
+                    document.getElementById(coordinate).style.color = outputColor; 
+                break;
+                case "2":
+                    outputColor = "green";
+                    document.getElementById(coordinate).innerText = content; 
+                    document.getElementById(coordinate).style.color = outputColor; 
+                break;
+                case "3":
+                    outputColor = "red";
+                    document.getElementById(coordinate).innerText = content; 
+                    document.getElementById(coordinate).style.color = outputColor; 
+                break;
+                case "4":
+                    outputColor = "dark-blue";
+                    document.getElementById(coordinate).innerText = content; 
+                    document.getElementById(coordinate).style.color = outputColor; 
+                break;
+                case "5":
+                    outputColor = "dark-red";
+                    document.getElementById(coordinate).innerText = content; 
+                    document.getElementById(coordinate).style.color = outputColor; 
+                break;
+                case "6":
+                    outputColor = "pink";
+                    document.getElementById(coordinate).innerText = content; 
+                    document.getElementById(coordinate).style.color = outputColor; 
+                break;
+                case "7":
+                    outputColor = "yellow";
+                    document.getElementById(coordinate).innerText = content; 
+                    document.getElementById(coordinate).style.color = outputColor; 
+                break;
+                case "8":
+                    outputColor = "purple";
+                    document.getElementById(coordinate).innerText = content; 
+                    document.getElementById(coordinate).style.color = outputColor; 
+                break;
+                case "B":
+                    outputColor = "black"
+                    document.getElementById(coordinate).innerText = content; 
+                    document.getElementById(coordinate).style.color = outputColor; 
+                break;
+                case "0":
+                    document.getElementById(coordinate).style.backgroundColor = "#B2B8B8";
+                    outputColor = "black";
+                break;
+                default:
+                    throw `coordinate ${coordinate} content ${content}`
+            }
+        }
+
     //class to represent each tile to limit interactions with the elements
     class tileObject {
         content;
@@ -48,36 +108,41 @@
             this.coordinate = inputCoordinate;
         }
         countSurroundingBombs(){
-            const surroundingTilesArray = [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]];
-            let XCoord = Number(this.coordinate.slice(0,2));
-            let YCoord = Number(this.coordinate.slice(2,4));
-            let netSurroundingBombs = 0;
-            surroundingTilesArray.forEach((x) => {
-                let XCoordToCheck = XCoord + x[0];
-                let YCoordToCheck = YCoord + x[1];
-                let CoordToCheck = digitize(XCoordToCheck) + digitize(YCoordToCheck);
-                //checks the resulting coordinate to see if it's within the boundaries, i.e the top left corner will not have a coordinate to its left or top
-                if(checkCoordinates(CoordToCheck)){
-                    //checks if the resulting coordinate has a bomb. if it does, increment netSurroundingBombs
-                    if(document.getElementById(CoordToCheck).innerText === 'B'){
-                        netSurroundingBombs++;
+            if (this.content !== "B"){
+                const surroundingTilesArray = [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]];
+                let XCoord = Number(this.coordinate.slice(0,2));
+                let YCoord = Number(this.coordinate.slice(2,4));
+                let netSurroundingBombs = 0;
+                surroundingTilesArray.forEach((x) => {
+                    let XCoordToCheck = XCoord + x[0];
+                    let YCoordToCheck = YCoord + x[1];
+                    let coordToCheck = digitize(XCoordToCheck) + digitize(YCoordToCheck);
+                    //checks the resulting coordinate to see if it's within the boundaries, i.e the top left corner will not have a coordinate to its left or top
+                    if(checkCoordinates(coordToCheck)){
+                        //checks if the resulting coordinate has a bomb. if it does, increment netSurroundingBombs
+                        if(getTileObject(coordToCheck).content === 'B'){
+                            netSurroundingBombs++;
+                        }
                     }
-                }
-            })
-            this.content = netSurroundingBombs.toString();
+                })
+                this.content = netSurroundingBombs.toString();
+            }
         }
         clickTile(){
-            if(this.content = "B") {
-                //gameOver();
+            this.clickstate = true;
+            if(this.content === "B") {
+                console.log('gameOver()');
+                bombLocations.forEach((x) => revealTile(x,"B"));
             } else {
-                document.getElementById(this.coordinate).innerText = this.content.toString();
+                revealTile(this.coordinate,this.content);
                 if(this.content === "0") {
                     getSurroundingTiles(this.coordinate).forEach((x) => {
-                        getTileObject(x).clickTile;
+                        if(!getTileObject(x).clickstate){
+                            getTileObject(x).clickTile();
+                        }
                     })
                 }
             }
-            this.clickstate = true;
         }
     }
     //Main functions
@@ -119,22 +184,31 @@
             var bombLocations = [];
             function gridSetup(numOfBombs){
                 for(i = 0 ; i < numOfBombs ; i++){
-                    let XCoordOfBomb = Math.floor(Math.random() * (gridWidth - 0.999) + 1);
-                    let YCoordOfBomb = Math.floor(Math.random() * (gridHeight - 0.999) + 1);
-                    let coordOfBomb = digitize(XCoordOfBomb) + digitize(YCoordOfBomb);
+                    let coordOfBomb;
+                    do {
+                        let XCoordOfBomb = Math.floor(Math.random() * (gridWidth - 0.999) + 1);
+                        let YCoordOfBomb = Math.floor(Math.random() * (gridHeight - 0.999) + 1);
+                        coordOfBomb = digitize(XCoordOfBomb) + digitize(YCoordOfBomb);
+                    } while (bombLocations.includes(coordOfBomb));
                     getTileObject(coordOfBomb).content = "B";
                     bombLocations.push(coordOfBomb);
                 }
                 generatedTileObjects.forEach((tileObj) => {
                     tileObj.countSurroundingBombs();
                 })
+                generatedTileObjects.forEach((x) => {
+                    x.countSurroundingBombs();
+                })
             }
+
+        //sets all tile objects click states to false, content to null, reset innertext and background color and reset the image
+        function startGame(){
+            
+        } 
 
 document.getElementById('gridContainer').appendChild(generateGrid(10,10));
 gridSetup(10);
-generatedTileObjects.forEach((x) => {
-    x.countSurroundingBombs();
-})
+document.getElementById('faceImage').style.display = "block";
 
 //event listeners
     //right click to "flag" the tile, turn the color red
@@ -157,9 +231,27 @@ generatedTileObjects.forEach((x) => {
     document.body.addEventListener('click',(event) => {
         if(event.target.className === 'gridTile'){
             let targetObject = getTileObject(event.target.id);
-            console.log(targetObject);
             if(!targetObject.clickstate){
                 targetObject.clickTile();
             }
         }
     })
+
+    //trigger the click image on mousedown and reverse on mouseup
+    document.getElementById("gridContainer").addEventListener('mousedown',() => {
+        document.getElementById('faceImage').style.display = "none";
+        document.getElementById('clickImage').style.display = "block";
+    })
+
+    document.getElementById("gridContainer").addEventListener("mouseup",() => {
+        document.getElementById('clickImage').style.display = "none";
+        document.getElementById('faceImage').style.display = "block";
+    })
+
+//debugging functions
+    //reveals all tile contents without triggering the clickTile() method
+    function debugReveal() {
+        generatedTileObjects.forEach((x) => {
+            document.getElementById(x.coordinate).innerText = x.content;
+        })
+    }
